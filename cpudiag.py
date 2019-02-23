5790a9b92a77c93f5baef6b540913f7ec9c529d7
Jacob Gray <officialjacobgray@gmail.com>
2019-02-23 15:48:48 -0800

    Edited license header formatting

--- cpudiag.py
+++ cpudiag.py
@@ -1,10 +1,11 @@
-''' Python 8080, an 8080 emulator designed for arcade games.
+''' 
+    python8080, an 8080 emulator designed for arcade games.
     Copyright (C) 2019  Jacob Gray
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,7 +13,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    along with this program.  
+    If not, see <https://www.gnu.org/licenses/>.
 '''
 
 import emu8080.emulator_8080 as emulator_8080
